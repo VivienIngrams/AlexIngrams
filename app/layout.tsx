7ad758cafd4b3aa3460002fb3b8da454949b8e82
VivienIngrams <vivingrams@gmail.com>
2023-09-01 15:01:06 +0100

    Adding layout wrapper and styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,54 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Link from "next/link";
+// import Logo from './alpha-logo.svg'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Alex Ingrams',
-  description: 'Assistant professor in the Institute of Public Administration at Leiden University, researching the role of transparency and technology in public administration and policy',
-}
+  title: "Alex Ingrams",
+  description:
+    "Assistant professor in the Institute of Public Administration at Leiden University, researching the role of transparency and technology in public administration and policy",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex h-screen flex-col justify-between pt-5 text-yellow-600 sm:pb-10">
+          <header className="flex w-full items-center justify-between">
+            <div>
+              <Link href="/" aria-label='Alex-Homepage'>
+                <div className="flex items-center justify-between">
+                  <div className="mr-3">
+                    {/* <Logo /> */}
+                  </div>
+                </div>
+              </Link>
+            </div>
+            <div className="flex items-center text-base  leading-5">
+              {/* <div className="">
+              {headerNavLinks.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="p-1 font-semibold uppercase text-yellow-600 dark:text-gray-100 sm:p-4 sm:text-xl"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div> */}
+            </div>
+          </header>
+          <main>{children}</main>
+          {/* <Footer /> */}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        playfair: ['Playfair Display Variable', 'sans'],
+        khand: ['Khand', 'sans'],
+      },
     },
   },
   plugins: [],
